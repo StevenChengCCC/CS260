8824673f39f384323f9eeb55c4ff489d6b6c55d1
StevenChengCCC <chengssh@byu.edu>
2024-12-11 22:30:27 -0700

    Forward messages to everyone except the sender

--- Anti-Scam/service/peerProxy.js
+++ Anti-Scam/service/peerProxy.js
@@ -17,6 +17,45 @@ function peerProxy(httpServer) {
   
   let connections = [];
 
+  wss.on('connection', (ws) => {
+    const connection = { id: uuid.v4(), alive: true, ws: ws };
+    connections.push(connection);
+    ws.on('message', function message(data) {
+      connections.forEach((c) => {
+        if (c.id !== connection.id) {
+          c.ws.send(data);
+        }
+      });
+    });
+
+    // Remove the closed connection so we don't try to forward anymore
+    ws.on('close', () => {
+      const pos = connections.findIndex((o) => o.id === connection.id);
+      if (pos >= 0) {
+        connections.splice(pos, 1);
+      }
+    });
+
+    // Respond to pong messages by marking the connection alive
+    ws.on('pong', () => {
+      connection.alive = true;
+    });
+  });
+
+  // Keep active connections alive
+  setInterval(() => {
+    connections.forEach((c) => {
+      // Kill any connection that didn't respond to the ping last time
+      if (!c.alive) {
+        c.ws.terminate();
+      } else {
+        c.alive = false;
+        c.ws.ping();
+      }
+    });
+  }, 10000);
+}
+
 
 
 module.exports = { peerProxy };
